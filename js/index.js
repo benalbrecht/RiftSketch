85c832ee597472eadab70a0aeccf590740f19340
Benjamin Albrecht <git@benalbrecht.de>
2015-01-16 16:12:23 +0100

    remove getusermedia

--- js/index.js
+++ js/index.js
@@ -55,20 +55,6 @@ function (
       // TODO: lol, this controller is out of control. Refactor and maybe actually
       // use Angular properly.
 
-      var setupVideoPassthrough = function () {
-        navigator.getUserMedia = navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
-        navigator.getUserMedia(
-          {video: true},
-          function (stream) {
-            var monitor = document.getElementById('monitor');
-            monitor.src = window.URL.createObjectURL(stream);
-          },
-          function () {
-            // video pass-through is optional.
-          }
-        );
-      };
-      setupVideoPassthrough();
 
       var setupSketch = function () {
         var autosave = localStorage.getItem('autosave');
